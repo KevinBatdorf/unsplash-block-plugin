ef5395d4b4b73613eb2d279dc592c918c328cc42
Kevin Batdorf <kbat82@gmail.com>
2022-06-06 02:52:32 -0400

    tweak icon color

--- src/icons.tsx
+++ src/icons.tsx
@@ -14,7 +14,31 @@ export const blockIcon = () => (
             d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
         />
         <path
-            fill="#cdcdcd"
+            fill="#ffffff"
+            // strokeWidth={1}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+    </svg>
+)
+export const blockIconPinkMiddle = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#1e1e1e"
+        strokeWidth={2}>
+        <path
+            fill="#ff77d9"
+            // fill="transparent"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
+        />
+        <path
+            fill="#ffbfbf"
             // strokeWidth={1}
             strokeLinecap="round"
             strokeLinejoin="round"
