15508d9cba5643354fd78e8eb0443a91ebf27189
Kevin Batdorf <kbat82@gmail.com>
2022-06-08 13:22:18 -0400

    Update version number

--- unlimited-photos.php
+++ unlimited-photos.php
@@ -4,7 +4,7 @@
  * Description:       Search over 3 million photos, textures, wallpapers, and more.
  * Requires at least: 6.0
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           1.0.0
  * Author:            Kevin Batdorf
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
