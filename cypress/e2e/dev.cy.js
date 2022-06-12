2c1d237344d509ccb68eef4f08d8046100d8592f
Kevin Batdorf <kbat82@gmail.com>
2022-06-12 22:07:37 +0700

    Fix confusing comment

--- cypress/e2e/dev.cy.js
+++ cypress/e2e/dev.cy.js
@@ -1,6 +1,6 @@
 context('Dev mode checks', () => {
     it("Make sure the modal isn't open on page load", () => {
-        // Adds our block
+        // Adds core image block
         cy.addBlock('editor-block-list-item-image')
 
         // Check the search bar is not there
