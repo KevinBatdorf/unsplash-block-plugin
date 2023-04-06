0789e8302c939665bbf68d48c2810314f5b2cb8a
Kevin Batdorf <kbat82@gmail.com>
2023-04-06 18:07:24 -0400

    update test

--- cypress/support/globals.js
+++ cypress/support/globals.js
@@ -1,12 +1,3 @@
-// Preserve WP cookies
-Cypress.Cookies.defaults({
-    preserve: /wordpress/,
-})
-
-// Make sure no errors
-Cypress.on('window:before:load', (win) => {
-    cy.spy(win.console, 'error')
-})
 afterEach(() => {
     cy.window().then((win) => {
         expect(win.console.error).to.have.callCount(0)
