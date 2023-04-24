563e270702a9ccea1939826f4e95e6ab50ce6c5f
Kevin Batdorf <kbat82@gmail.com>
2023-04-23 21:42:49 -0400

    Add new tests

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,2 @@
-// export const API_URL = 'https://api.example.com'
+export const API_URL = 'https://unsplash-api-search.vercel.app/'
 // export const API_URL = 'http://localhost:3001'
-export const API_URL =
-    'https://unsplash-api-git-add-lexica-support-kevinbatdorf.vercel.app'
